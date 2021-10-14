85c2a56da81ba8bafe82d2cc809889b5f73cbe7f
luiscostalafc <Nanico2406>
2021-10-13 22:45:17 -0300

    feat: create project

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>React Native</Text>
+      <Text>Ignite</Text>
+    </View>
+  );
+};
+
+export { Home };
